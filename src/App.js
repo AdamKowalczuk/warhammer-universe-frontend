26e05c5a33d82d6de5125b78d73050edaacec745
Adam <adam.kowalczuk@poczta.fm>
2021-05-31 15:26:25 +0200

    commit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import "./App.css";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 import History from "./views/history";
 import Fractions from "./views/fractions";
@@ -14,14 +14,16 @@ function App() {
     <Router>
       <Nav />
       <History />
-      <Route exact path="/historia">
-        <History />
-      </Route>
-      <Route path="/" exact component={ExercisesList} />
-      <Route path="/posty/:id" component={Details} />
-      {/* <Route path="/historia" exact component={History} /> */}
-      <Route path="/frakcje" exact component={Fractions} />
-      <Route path="/dodaj-post" component={AddPost} />
+      <Switch>
+        <Route exact path="/historia">
+          <History />
+        </Route>
+        <Route path="/" exact component={ExercisesList} />
+        <Route path="/posty/:id" component={Details} />
+        {/* <Route path="/historia" exact component={History} /> */}
+        <Route path="/frakcje" exact component={Fractions} />
+        <Route path="/dodaj-post" component={AddPost} />
+      </Switch>
       <footer>
         <h2>Copyright &copy; Warhammer Universe 2021</h2>
       </footer>
